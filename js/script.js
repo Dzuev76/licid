6546e891f1cf23d925a7697ed2e20760117456bd
Dzuev76 <Dzuev76@gmail.com>
2018-06-14 14:07:11 +0600

    change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
+$(document).ready(function(){
+        $("#anchor-1").on("click", function(){
+            var scroll=$("#features").offset().top;
+            $('html, body').animate({scrollTop: scroll}, 3000);
+        });
+    });
 
+$(document).ready(function(){
+        $("#anchor-2").on("click", function(){
+            var scroll=$("#about").offset().top;
+            $('html, body').animate({scrollTop: scroll}, 3000);
+        });
+    });
 
+$(document).ready(function(){
+        $("#anchor-3").on("click", function(){
+            var scroll=$("#testimonials").offset().top;
+            $('html, body').animate({scrollTop: scroll}, 3000);
+        });
+    });
+
+$(document).ready(function(){
+        $("#anchor-4").on("click", function(){
+            var scroll=$("#pricin").offset().top;
+            $('html, body').animate({scrollTop: scroll}, 3000);
+        });
+    });
+
+$(document).ready(function(){
+        $("#anchor-5").on("click", function(){
+            var scroll=$("#contact").offset().top;
+            $('html, body').animate({scrollTop: scroll}, 3000);
+        });
+    });
 
 
 
